1f90899eb928238d236abe0da84976e4f495cd07
Bruno <brunolimadevelopment@gmail.com>
2023-06-25 13:55:54 -0300

    procedure exemplo 2

---
+++ procedure2.pas
@@ -0,0 +1,10 @@
+{$CODEPAGE UTF8}
+program mensagem;
+procedure Mensagem();
+    begin
+        writeln('Olá, você está estudando programação na Área dev.');
+    end;
+
+begin
+    Mensagem();
+end.
